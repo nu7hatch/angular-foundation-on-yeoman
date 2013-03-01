2f1f3f892958592f4b2829fef033e023e0c65a88
Cubox <dev@cuboxsa.com>
2013-03-01 02:41:16 -0200

    fixed testacular conf

--- config/testacular.conf.js
+++ config/testacular.conf.js
@@ -7,12 +7,12 @@ basePath = '';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'app/components/angular/angular.js',
-  'app/components/angular-mocks/angular-mocks.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js'
+  '../app/components/angular/angular.js',
+  '../app/components/angular-mocks/angular-mocks.js',
+  '../app/scripts/*.js',
+  '../app/scripts/**/*.js',
+  '../test/mock/**/*.js',
+  '../test/spec/**/*.js'
 ];
 
 // list of files to exclude
