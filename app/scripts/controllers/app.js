c76fea746cb972a85ce1bf14b09db15a4d6b2ae3
Cubox <dev@cuboxsa.com>
2013-03-02 16:28:55 -0200

    improved i18n extension, got rid of $rootScope usage + docs"

--- app/scripts/controllers/app.js
+++ app/scripts/controllers/app.js
@@ -3,6 +3,9 @@
 app.controller('AppCtrl', function($scope, $rootScope, $route, $log, i18n) {
     var DEFAULT_LAYOUT = 'views/layouts/default.html';
 
+    // Initialize I18n extension and load a dictionary for current locale.
+    i18n.init();
+    
     // Configure layout and templates.
     $rootScope.$on('$routeChangeStart', function(scope, next, current) {
         var route = next.$route;
--- app/scripts/extensions/i18n.js
+++ app/scripts/extensions/i18n.js
@@ -1,64 +1,182 @@
 'use strict';
 
-// Define I18n factory which is going to load current locale data
-// from served resources.
-app.factory('i18n', function I18n($locale, $log, $http, $interpolate, $rootScope) {
-    var dict = {}
-      , currentLocale = $locale.id
-      , defaultLocale = 'en';
-    
-    (function loadDict(locale, ignoreError) {
-        var url =  '/locales/' + locale + '.json';
-        
-        $http({method: 'GET', url: url, cache: true})
-            .error(function() {
-                $log.error("Couldn't get language data for: " + locale);
-                
-                if (!ignoreError) {
-                    $log.info("Falling back to default locale");
-                    loadDict(defaultLocale, true);
-                }
-            })
-            .success(function(data) {
-                dict = data;
-                
-                $log.info('Language data loaded for "' + locale + '"');
-                $rootScope.i18nReady = true;
-                $rootScope.$broadcast('languageDataLoaded');
-            });
-    })(currentLocale, false);
+/**
+ * @ngdoc factory
+ * @name ng.factory:Directive
+ *
+ * @description
+ * This is an entity for I18n dictionaries. This factory shouldn't be
+ * accessed directly - it is ment to be used internally by the `i18n`
+ * translation service.
+ */
+app.factory('Dictionary', function($http, $locale, $log) {
+    return {
+        get: function(localeId, fallback, callback) {
+            var dictUrl = '/locales/' + localeId + '.json'
+              , self = this;
+            
+            $http({method: 'GET', url: dictUrl, cache: true})
+                .error(function() {
+                    $log.error('Couldn\'t get language data for "' + localeId + '"');
+                    
+                    if (fallback != undefined) {
+                        $log.info('Falling back to default locale "' + localeId + '"');
+                        self.get(fallback, undefined, callback);
+                    }
+                })
+                .success(function(data) {
+                    $log.info('Locale data dictionary loaded for "' + localeId + '"');
+                    callback(data);
+                });
+        }
+    };
+});
 
-    $rootScope.i18n = {
+/**
+ * @ngdoc service
+ * @name ng.service:i18n
+ *
+ * @description
+ * I18n service is responsible for all the localization stuff along your
+ * application. It loads locale dictionaries and provides bunch of functions
+ * to translate your texts.
+ *
+ * # Usage
+ *
+ * Inject `i18n` to your application runner or main controller and initialize
+ * dictionaries:
+ *
+ * <pre>
+ * app.run(['i18n', function(i18n) {
+ *   i18n.init();
+ * }]);
+ * </pre>
+ *
+ * You can also pass initial locale to `i18n.init()`, just like here:
+ *
+ * <pre>
+ * i18n.init('en-us');
+ * </pre>
+ *
+ * Eventually, you can change language at any time:
+ *
+ * <pre>
+ * i18n.setLocale('en-us');
+ * </pre>
+ *
+ * To translate your stuff you have `translate` and `translateN` functions
+ * (with shortcuts `t` and `tn`) available.
+ *
+ * <pre>
+ * i18n.t('Hello, {{user}}!', {user: 'John'});
+ * i18n.tn('You have {} components installed!', components.length);
+ * </pre>
+ *
+ * Both functions take a scope as last argument. 
+ */
+app.service('i18n', function($locale, $interpolate, $rootScope, Dictionary) {
+    var DEFAULT_LOCALE = 'en';
+    
+    return {
+        dict: {},
+        currentLocale: $locale.id,
+
+        init: function(localeId) {
+            if (localeId) {
+                this.currentLocale = localeId;
+            }
+            
+            this.loadDictionary();
+        },
+        setLocale: function(localeId) {
+            this.currentLocale = localeId;
+            this.loadDictionary();
+            
+            return localeId;
+        },
+        loadDictionary: function() {
+            var self =  this;
+            $rootScope.i18nReady = false;
+
+            Dictionary.get(this.currentLocale, DEFAULT_LOCALE, function(dict) {
+                self.dict = dict;
+                
+                $rootScope.i18nReady = true;
+                $rootScope.$broadcast('localeDictionaryLoaded');
+            });
+
+            return this.dict;
+        },
         getTranslation: function(key) {
-            return dict[key];
+            return this.dict[key];
         },
         getTranslationN: function(key, count) {
-            var whens = dict[key];
-            if (whens == undefined || !!whens) return key;
+            var whens = this.dict[key];
+
+            if (whens == undefined || !!whens) {
+                return key;
+            }
+            
             return whens[count] || whens[$locale.pluralCat(count)] || key;
         },
         translate: function(key, scope) {
-            var translation = getTranslation(key) || key;
+            var translation = this.getTranslation(key) || key;
             return $interpolate(translation, scope);
         },
         t: function(key, scope) {
             return this.translate(key, scope);
         },
         translateN: function(key, count, scope) {
-            var translation = getTranslationN(key, count) || key;
+            var translation = this.getTranslationN(key, count) || key;
             return $interpolate(translation, scope);
         },
         tn: function(key, count, scope) {
-            return this.pluralTranslate(key, scope);
+            return this.translateN(key, scope);
         }
     };
-}).$inject = ['$locale', '$log', '$http', '$interpolate', '$rootScope'];
+}).$inject = ['$locale', '$interpolate', '$rootScope', 'Dictionary'];
 
-// Define a translation directive.
-app.directive('t', function($rootScope, $interpolate) {
+/**
+ * @ngdoc directive
+ * @name ng.directive:t
+ * @restrict A
+ *
+ * @description
+ * Gettext-like translation directive.
+ *
+ * # Usage
+ * It's simple, you just specify your translation which includes all the
+ * interpolated stuff in a `t` attribute:
+ *
+ * <pre>
+ * <h1 t="Hello {{userName}}!">Hello John!</h1>
+ * </pre>
+ *
+ * The content of the element is just an example text.
+ *
+ * # Pluralization
+ * Pluralization works very similar to `ngPluralize` directive from Angular.
+ * The only difference is that you don't have to specify all the whens in
+ * your view. You give only one key translation in `t` attribute and specify
+ * number which is going to pluralize this message in `t-plural` attribute.
+ *
+ * <pre>
+ * <h2 t="You have {} installed components" t-plural="components.length">
+ *   You have 3 installed components
+ * </h2>
+ * </pre>
+ *
+ * Translated message, similary to `ngPluralize` will include pluralization
+ * counter in the place of `{}` brackets.
+ *
+ * @param {string} t The text to be translated (a translation key).
+ * @param {string|expression} plural The variable to be bounded to when pluralize.
+ */
+app.directive('t', function($rootScope, $interpolate, i18n) {
     var BRACE = /{}/g;
     return {
         restrict: 'A',
+        
         link: function(scope, element, attr) {
             var template = element.attr(attr.$attr.t)
               , countExp = attr.tPlural
@@ -72,10 +190,10 @@ app.directive('t', function($rootScope, $interpolate) {
                       , startSymbol = $interpolate.startSymbol()
                       , endSymbol = $interpolate.endSymbol();
                     
-                    var t = $rootScope.i18n.getTranslationN(template, count);
+                    var t = i18n.getTranslationN(template, count);
                     translation = t.replace(BRACE, startSymbol + countExp + endSymbol);
                 } else {
-                    translation = $rootScope.i18n.getTranslation(template);
+                    translation = i18n.getTranslation(template);
                 }
 
                 if (!!translation) {
@@ -85,7 +203,7 @@ app.directive('t', function($rootScope, $interpolate) {
                 }
             };
 
-            scope.$on('languageDataLoaded', callback);
+            scope.$on('localeDictionaryLoaded', callback);
             attr.$observe('t', callback);
 
             if (isPlural) {
@@ -93,4 +211,4 @@ app.directive('t', function($rootScope, $interpolate) {
             }
         }
     };
-}).$inject = ['$rootScope', '$interpolate'];
+}).$inject = ['$rootScope', '$interpolate', 'i18n'];
